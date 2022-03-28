b69b2e7a9e2ab577003f5a8a759d65d1fd0d106a
setiwannurarif <61929573+setiawannurarif@users.noreply.github.com>
2022-03-28 18:54:37 +0700

    update path

--- res/js/app.js
+++ res/js/app.js
@@ -1,1 +1,10 @@
 console.log('Javascript has been loaded!');
+
+let score_luckycat;
+let score_player;
+let timeout;
+
+const btn_play = document.querySelector('#play');
+btn_play.addEventListener('click', () => {
+  console.log('PLAY!');
+});
